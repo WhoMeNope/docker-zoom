ff5edd8c2e372c0569d5ad0ab74b1ba071660b20
Martin Toman <whomenope@outlook.com>
2020-05-06 15:06:42 +0200

    Add start script

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -ex
+
+xhost local:zoom
+
+if [ "$1" == "-v" ]; then
+  docker run --rm --name zoom -d -e DISPLAY=$DISPLAY --device /dev/video0:/dev/video0 -v /tmp/.X11-unix:/tmp/.X11-unix -v /run/user/1000/pulse:/run/pulse zoom
+else
+  docker run --rm --name zoom -d -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /run/user/1000/pulse:/run/pulse zoom
+fi
