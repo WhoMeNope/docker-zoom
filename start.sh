858485ced6e5f0d24facc7e503b6ba36e2141b4e
Martin Toman <toman.martin@live.com>
2020-11-13 14:30:15 +0100

    Save user preferences + keep me logged in

--- start.sh
+++ start.sh
@@ -4,8 +4,9 @@ set -ex
 
 xhost local:zoom
 
+DEVICE=""
 if [ "$1" == "-v" ]; then
-  docker run --rm -d -e DISPLAY=$DISPLAY --device /dev/video0:/dev/video0 -v /tmp/.X11-unix:/tmp/.X11-unix -v /run/user/1000/pulse:/run/pulse zoom-new
-else
-  docker run --rm -d -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /run/user/1000/pulse:/run/pulse zoom-new
+  DEVICE+="--device /dev/video0:/dev/video0"
 fi
+
+docker run --rm -d -e DISPLAY=$DISPLAY $DEVICE -v $(pwd)/.zoom:/home/developer/.zoom -v /tmp/.X11-unix:/tmp/.X11-unix -v /run/user/1000/pulse:/run/pulse zoom-new
